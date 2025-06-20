8484e0cd94a32d7ef6f7e4b0510e579387b5d7c4
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-06-20 15:13:53 -0700

    Initial Commit

---
+++ TableTopTimer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TableTopTimer
+//
+//  Created by Yuhao Chen on 6/20/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
